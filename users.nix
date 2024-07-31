6c9d36f059f3322e320ac27915737b0f62ca1885
Makrennel <makrommel@protonmail.ch>
2024-07-31 17:47:11 +0100

    Switch to using runCommand to create skeleton

--- users.nix
+++ users.nix
@@ -1,13 +1,30 @@
 { lib, ... }: let
 	usersFile = pkgs.runCommand "get-declared-users" {} "ls -1 ${users.source} > $out";
 	usersList = lib.lists.filter (e: ! (e == "" || e == [])) (lib.strings.split "\n" (builtins.readFile usersFile));
+	skeleton = pkgs.runCommand "user-skeleton" {} ''
+		mkdir -p $out/{Desktop,Documents,Downloads,Local,Music,Images,Repositories,Shared,Templates,Videos} &&
+		mkdir -p $out/Local/{bin,cache,etc,share,state,.var} &&
+		mkdir -p $out/Local/etc/git &&
+		mkdir -p $out/Local/share/{fonts,icons,themes} &&
+		ln -s share/fonts $out/Local/.fonts &&
+		ln -s share/icons $out/Local/.icons &&
+		ln -s share/themes $out/Local/.themes &&
+		ln -s .var $out/Local/var &&
+		ln -s bin $out/Local/.bin &&
+		ln -s cache $out/Local/.cache &&
+		ln -s etc $out/Local/.config &&
+		ln -s . $out/Local/.local &&
+		ln -s .. $out/Local/home &&
+		touch $out/Local/etc/git/config &&
+		echo 'wget --hsts-file="$XDG_CACHE_HOME/wget-hsts"' > $out/Local/etc/wgetrc
+	'';
 in {
 	options.users.source = lib.mkOption {
 		type = lib.types.str;
 	};
 
 	security.pam.makeHomeDir = true;
-	security.pam.makeHomeDir.skelDirectory = users.source;
+	security.pam.makeHomeDir.skelDirectory = skeleton;
 
 	users.users = foldl (a: b: a // b) {} {
 		lib.lists.forEach usersList (user: lib.nameValuePair user {
