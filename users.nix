4cf9a7e806644700871a0b92b4a8cad2730b1480
Makrennel <makrommel@protonmail.ch>
2024-08-01 01:53:29 +0100

    Manage user-dirs.dirs automatically

--- users.nix
+++ users.nix
@@ -49,6 +49,7 @@ in {
 			xdg.dataHome = "/users/${config.home.username}/Local/share";
 			xdg.stateHome = "/users/${config.home.username}/Local/state";
 
+			xdg.userDirs.enable = true;
 			xdg.userDirs.desktop = "/users/${config.home.username}/Desktop";
 			xdg.userDirs.documents = "/users/${config.home.username}/Documents";
 			xdg.userDirs.pictures = "/users/${config.home.username}/Images";
@@ -56,6 +57,12 @@ in {
 			xdg.userDirs.publicShare = "/users/${config.home.username}/Shared";
 			xdg.userDirs.templates = "/users/${config.home.username}/Templates";
 			xdg.userDirs.videos = "/users/${config.home.username}/Videos";
+			xdg.userDirs.extraConfig = {
+				XDG_REPOSITORY_DIR = "/users/${config.home.username}/Repositories";
+				XDG_FILES_HOME = "/users/${config.home.username}";
+				XDG_VAR_HOME = "/users/${config.home.username}/Local/.var";
+				XDG_BIN_HOME = "/users/${config.home.username}/Local/bin";
+			};
 
 			home.preferXdgDirectories = true;
 		})];
